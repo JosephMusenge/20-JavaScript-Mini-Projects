88e5a7162e94830d93a6ef6cc057d9af0b116efa
Joseph Musenge <josephmusengep62@gmail.com>
2023-01-22 19:39:21 -0600

    add comments to code

--- 07 - Sidebar/sidebar.js
+++ 07 - Sidebar/sidebar.js
@@ -1,7 +1,10 @@
+// Try to work with the code of the last project as an example
+// Get all necessary classes and store them in variables
 const toggleBtn = document.querySelector(".sidebar-toggle");
 const closeBtn = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
 
+// Add click event on the toggle button
 toggleBtn.addEventListener("click", function() {
     // if (sidebar.classList.contains("show-sidebar")) {
     //     sidebar.classList.remove("show-sidebar");
@@ -10,9 +13,10 @@ toggleBtn.addEventListener("click", function() {
     //     sidebar.classList.add("show-sidebar");
     // }
 
-    sidebar.classList.toggle("show-sidebar");
+    sidebar.classList.toggle("show-sidebar"); // This single line does everything that the above code does
 })
 
+// Add close click event on the close buttn
 closeBtn.addEventListener("click", function() {
     sidebar.classList.remove("show-sidebar");
 })

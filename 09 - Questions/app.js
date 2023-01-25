36f38ac97ef0be1901d5370a91e38cd27b244a00
Joseph Musenge <josephmusengep62@gmail.com>
2023-01-24 23:43:50 -0600

    add JavaScript to questions project

--- 09 - Questions/app.js
+++ 09 - Questions/app.js
@@ -1,2 +1,13 @@
-//using selectors inside the element
-// traversing the dom
+// Solution one: traversing the dom
+const btns = document.querySelectorAll(".question-btn");
+
+// Loop over all the buttons
+btns.forEach(function(btn) {
+    // add a click event listener to every btn
+    btn.addEventListener("click", function(e) {
+        const questions = e.currentTarget.parentElement.parentElement; //Select parent of the parent element of the current btn
+        questions.classList.toggle("show-text");
+    })
+})
+
+// Solution two: using selectors inside the element

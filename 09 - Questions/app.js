36f3d7b0314eeb58cc234b1b4e266cb039a21dcb
Joseph Musenge <josephmusengep62@gmail.com>
2023-01-25 23:35:31 -0600

    update js: try second solution

--- 09 - Questions/app.js
+++ 09 - Questions/app.js
@@ -1,13 +1,30 @@
-// Solution one: traversing the dom
-const btns = document.querySelectorAll(".question-btn");
+// Solution one: using selectors inside the element
+const questions = document.querySelectorAll(".question");
 
-// Loop over all the buttons
-btns.forEach(function(btn) {
-    // add a click event listener to every btn
-    btn.addEventListener("click", function(e) {
-        const questions = e.currentTarget.parentElement.parentElement; //Select parent of the parent element of the current btn
-        questions.classList.toggle("show-text");
+questions.forEach(function (question) {
+    const btn = question.querySelector(".question-btn");
+
+    btn.addEventListener("click", function() {
+        // Close one btn if another is open
+        questions.forEach(function(item) {
+            if (item !== question) {
+                item.classList.remove("show-text");
+            }
+        })
+
+        question.classList.toggle("show-text");
     })
 })
 
-// Solution two: using selectors inside the element
+
+// Solution two: traversing the dom
+// const btns = document.querySelectorAll(".question-btn");
+
+// Loop over all the buttons
+// btns.forEach(function(btn) {
+//     // add a click event listener to every btn
+//     btn.addEventListener("click", function(e) {
+//         const questions = e.currentTarget.parentElement.parentElement; //Select parent of the parent element of the current btn
+//         questions.classList.toggle("show-text");
+//     })
+// })

8df4b8b2fc31c1b76ccb60f698ddd81333300d2b
Joseph Musenge <josephmusengep62@gmail.com>
2023-02-05 21:27:05 -0600

    changed giveaway timer using js

--- 14 - Countdown Timer/timer.js
+++ 14 - Countdown Timer/timer.js
@@ -21,3 +21,21 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+let futureDate = new Date(2023, 07, 29, 23, 59, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+let month = futureDate.getMonth();
+month = months[month];
+const date = futureDate.getDate();
+let day = weekdays[futureDate.getDay()];
+
+// const secs = futureDate.getSeconds();
+// Set giveaway date dynamically
+giveaway.textContent = `giveaway ends on ${day}, ${date} ${month} ${year} at ${hours}:${minutes}pm`;

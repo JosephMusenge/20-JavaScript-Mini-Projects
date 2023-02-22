24d51d2ed95dc89b84e35e0b19fe0684414f4a81
Joseph Musenge <josephmusengep62@gmail.com>
2023-02-22 00:28:34 -0600

    add javascript functionality

--- 18 - Dad Jokes Generator/app.js
+++ 18 - Dad Jokes Generator/app.js
@@ -1,1 +1,45 @@
 // JavaScript code starts here!
+
+// Get all variables
+const button = document.getElementById('jokeBtn');
+const jokeElement = document.getElementById('joke');
+
+// Add click event on button 
+button.addEventListener("click", generateJoke);
+generateJoke();
+
+async function generateJoke() {
+    const config = {
+        headers: {
+            'Accept': 'application/json',
+        },
+    };
+
+    // Request joke from API and load it using the fetch() method
+    const result = await fetch('https://icanhazdadjoke.com', config)
+
+    const data = await result.json();
+
+    jokeElement.innerHTML = data.joke;
+
+};
+
+
+/* ******** METHOD TWO *********** */
+
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             'Accept': 'application/json',
+//         },
+//     };
+
+//     fetch('https://icanhazdadjoke.com', config)
+//         .then((response) => response.json())
+//         .then((data) => {
+//             jokeElement.innerHTML = data.joke;
+//         })
+//         .catch(error => console.log(error));
+// }
+
+/* ***************************************************************************** */
